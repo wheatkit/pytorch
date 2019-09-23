057a0fdaad0d32006ca2904e5112bc785de07add
Raghu Krishnamoorthi <raghuramank@gmail.com>
2019-09-23 10:48:39 -0700

    Moving average for statistics in observers
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/26626
    
    
    ghstack-source-id: 90564535
    
    Differential Revision: [D17520344](https://our.internmc.facebook.com/intern/diff/D17520344/)

--- torch/quantization/observer.py
+++ torch/quantization/observer.py
@@ -170,6 +170,7 @@ class MinMaxObserver(ObserverBase):
         #  aten/src/ATen/native/quantized/cpu/qconv.cpp
         #  This is not the optimal choice for non x86 backends as
         #  lose a bit of precision for activations.
+        self.averaging_constant = kwargs.pop('averaging_constant', 0.01)
 
         super(MinMaxObserver, self).__init__(**kwargs)
         self.min_val = None
@@ -190,8 +191,10 @@ class MinMaxObserver(ObserverBase):
             min_val = torch.min(x)
             max_val = torch.max(x)
         else:
-            min_val = torch.min(torch.min(x), min_val)
-            max_val = torch.max(torch.max(x), max_val)
+            min_val = torch.min(x)
+            max_val = torch.max(x)
+            min_val = self.min_val + self.averaging_constant * (min_val - self.min_val)
+            max_val = self.max_val + self.averaging_constant * (max_val - self.max_val)
         self.min_val = min_val
         self.max_val = max_val
         return x
@@ -226,6 +229,8 @@ class PerChannelMinMaxObserver(ObserverBase):
     """
 
     def __init__(self, ch_axis=0, **kwargs):
+        self.averaging_constant = kwargs.pop('averaging_constant', 0.01)
+
         super(PerChannelMinMaxObserver, self).__init__(**kwargs)
         self.ch_axis = ch_axis
         self.min_vals = None
@@ -254,8 +259,10 @@ class PerChannelMinMaxObserver(ObserverBase):
                 min_vals = torch.min(y, 1)[0]
                 max_vals = torch.max(y, 1)[0]
             else:
-                min_vals = torch.min(torch.min(y, 1)[0], min_vals)
-                max_vals = torch.max(torch.max(y, 1)[0], max_vals)
+                min_vals = torch.min(y, 1)[0]
+                max_vals = torch.max(y, 1)[0]
+                min_vals = self.min_vals + self.averaging_constant * (min_vals - self.min_vals)
+                max_vals = self.max_vals + self.averaging_constant * (max_vals - self.max_vals)
             self.min_vals = min_vals
             self.max_vals = max_vals
             return x
@@ -298,6 +305,7 @@ class HistogramObserver(ObserverBase):
 
     def __init__(self, bins=2048, **kwargs):
         # bins: The number of bins used for histogram calculation.
+        self.averaging_constant = kwargs.pop('averaging_constant', 0.01)
         super(HistogramObserver, self).__init__(**kwargs)
         self.bins = bins
         self.histogram = None
@@ -501,11 +509,13 @@ class HistogramObserver(ObserverBase):
                 combined_histogram = torch.zeros_like(self.histogram)
                 combined_min = torch.min(new_min, self.min_val)
                 combined_max = torch.max(new_max, self.max_val)
+#                combined_min = self.min_val + self.averaging_constant * (new_min - self.min_val)
+#                combined_max = self.max_val + self.averaging_constant * (new_max - self.max_val)
                 self._combine_histograms(
                     combined_histogram,
                     combined_min.item(),
                     combined_max.item(),
-                    self.histogram,
+                    self.histogram * (1-self.averaging_constant),
                     self.min_val.item(),
                     self.max_val.item(),
                 )
@@ -513,7 +523,7 @@ class HistogramObserver(ObserverBase):
                     combined_histogram,
                     combined_min.item(),
                     combined_max.item(),
-                    new_histogram,
+                    self.averaging_constant * new_histogram,
                     new_min.item(),
                     new_max.item(),
                 )
@@ -593,7 +603,7 @@ def default_observer(**kwargs):
 
 def default_l2_observer(**kwargs):
     # Restrict activations to be in the range (0,127)
-    kwargs.setdefault("reduce_range", True)
+        kwargs.setdefault("reduce_range", True)
     return observer(HistogramObserver, **kwargs)
 
 def default_debug_observer(**kwargs):
